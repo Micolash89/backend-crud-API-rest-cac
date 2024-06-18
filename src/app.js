e37ba6683df5be383728f8d4de2d9853c61bccca
javier <espindolajavier2013@gmail.com>
2024-06-18 09:30:40 -0300

    "Updated package-lock.json and package.json files, added new dependencies, and removed index.js file."

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import config from "./config/config.js";
+import cors from "cors";
+
+const port = config.port || 8080;
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(cors({ origin: "*", methods: ["GET", "POST", "PUT", "DELETE"], credentials: true }));
+
+const server = app.listen(port, () => {
+    console.log(`Servidor escuchando en el puerto ${port}`);
+})
+

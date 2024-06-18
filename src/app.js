c4f2f4a582ec6e5fc747fc7d608d8a6a2a7fe732
javier <espindolajavier2013@gmail.com>
2024-06-18 10:21:05 -0300

    "Added import statement for db and uncommented code for database query, updated CORS configuration"

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import config from "./config/config.js";
 import cors from "cors";
+// import db from "./db/db.js"
 
 const port = config.port || 8080;
 
@@ -8,8 +9,14 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 app.use(cors({ origin: "*", methods: ["GET", "POST", "PUT", "DELETE"], credentials: true }));
+
+/*hacer otro archivo para que maneje la base de datos y no este todo en los controladores del router*/
+// db.query("SELECT * FROM alumnos", (err, result) => {
+//     if (err) throw err;
+//     console.log(result);
+//     console.log(typeof result)
+// })
 
 const server = app.listen(port, () => {
     console.log(`Servidor escuchando en el puerto ${port}`);

8b5d228a040b077fe11213e5308913c52690f5ec
javier <espindolajavier2013@gmail.com>
2024-06-30 00:32:24 -0300

    "Refactored alumnosController.js and alumnosRouter.js: updated function declarations, added error handling, and modified SQL queries and route paths."

--- src/controllers/alumnosController.js
+++ src/controllers/alumnosController.js
@@ -1,98 +1,101 @@
 import db from "../db/db.js"
 import { createHash } from "../../utils.js";
 
-export const obtenerListaAlumnos = (req,res)=>{
+export const obtenerListaAlumnos = (req, res) => {
     /*hacer otro archivo para que maneje la base de datos y no este todo en los controladores del router*/
-    db.query("SELECT * FROM alumnos", (err, result) => {
+    db.query("SELECT * FROM alumnos WHERE estado=1", (err, result) => {
         if (err) throw err;
         res.send({
-            message:"lisr de alumnos",
-            payload:result
+            message: "lisr de alumnos",
+            payload: result
         });
 
     })
 
- }
+}
 
- export const  subirAlumno = (req,res)=>{
+export const subirAlumno = (req, res) => {
 
-    const { nombre,apellido,email,fecha_nacimiento,password } = req.body;
+    const { nombre, apellido, email, fecha_nacimiento, password } = req.body;
 
-    if(!nombre || !apellido || !email || fecha_nacimiento || !password){
+    console.log(req.body);
+
+    if (!nombre || !apellido || !email || !fecha_nacimiento || !password) {
         return res.status(400).send({
-            message:"falta un campo al cargar",
-            payload:[]
+            message: "falta un campo al cargar",
+            payload: []
         })
     }
 
     const sql = "INSERT INTO alumnos (nombre,apellido,email,fecha_nacimiento,estado,password) VALUES (?,?,?,?,?,?)";
 
-    db.query(sql,[nombre,apellido,email,fecha_nacimiento,true, createHash(password)], (err, result) => {
+    db.query(sql, [nombre, apellido, email, fecha_nacimiento, true, createHash(password)], (err, result) => {
         if (err) throw err;
         res.send({
-            message:"carga exitosa de alumno",
-            payload:result.insertId
+            message: "carga exitosa de alumno",
+            payload: result.insertId
         });
-        
+
     })
 
- }
+}
 
- export const  actualizarAlumno = (req,res)=>{
+export const actualizarAlumno = (req, res) => {
 
-    const { id_alumno,nombre,apellido,email,fecha_nacimiento } = req.body;
+    const { id_alumno, nombre, apellido, email, fecha_nacimiento } = req.body;
 
-    if(!id_alumno || !nombre || !apellido || !email || fecha_nacimiento ){
+    if (!id_alumno || !nombre || !apellido || !email || !fecha_nacimiento) {
         return res.status(400).send({
-            message:"falta un campo al cargar",
-            payload:[]
+            message: "falta un campo al cargar",
+            payload: []
         })
     }
 
-    const sql = "UPDATE alumnos SET alumno=? , apellido=? , email=? , fecha_nacimiento=? WHERE id_alumno=?";
+    const sql = "UPDATE alumnos SET nombre=? , apellido=? , email=? , fecha_nacimiento=? WHERE id_alumno=?";
 
-    db.query(sql,[nombre,apellido,email,fecha_nacimiento,id_alumno], (err, result) => {
+    db.query(sql, [nombre, apellido, email, fecha_nacimiento, id_alumno], (err, result) => {
 
-        if (err) return res.status(500).send({
-            message:"error en la base de datos",
-            payload:[]
-        });
+        if (err) {
+            return res.status(500).send({
+                message: "error en la base de datos",
+                payload: []
+            });
+        }
 
         return res.send({
-            message:"Actualización de alumno exitosa",
-            payload:result.info
+            message: "Actualización de alumno exitosa",
+            payload: result.info
         });
-        
+
     });
 
- }
+}
 
 
- export const  eliminarAlumno = (req,res)=>{
+export const eliminarAlumno = (req, res) => {
 
-    const { id_alumno} = req.body;
-
-    if(!id_alumno ){
+    const id_alumno = req.params.aid;
+    if (!id_alumno) {
         return res.status(400).send({
-            message:"falta un campo al cargar",
-            payload:[]
+            message: "falta un campo al cargar",
+            payload: []
         })
     }
 
-    const sql = "UPDATE alumnos SET estado=? WHERE id_alumno=?";
+    const sql = "UPDATE alumnos SET estado=? WHERE id_alumno=? ";
 
-    db.query(sql, [false,id_alumno], (err, result) => {
+    db.query(sql, [false, id_alumno], (err, result) => {
 
         if (err) return res.status(500).send({
-            message:"error en la base de datos",
-            payload:[]
+            message: "error en la base de datos",
+            payload: []
         });
 
         return res.send({
-            message:"Se elimió el alumno de forma exitosa",
-            payload:result.info
+            message: "Se elimió el alumno de forma exitosa",
+            payload: result.info
         });
-        
+
     });
 
- }
+}
